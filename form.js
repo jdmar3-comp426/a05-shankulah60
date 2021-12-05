452b567030873cbbacf12ebf99c18517b6fa8718
shankulah60 <49954789+shankulah60@users.noreply.github.com>
2021-12-04 19:50:59 -0500

    Added form.js

---
+++ form.js
@@ -0,0 +1,6 @@
+function submit_form()
+{
+    var usr = document.getElementById("username_box").value;
+    var pass = document.getElementById("password_box").value;
+    document.getElementById("output").value = usr;
+}
